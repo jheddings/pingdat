36bf9b8630a7813b40fa887119920e1200ae782d
jheddings <jason@heddings.com>
2023-03-16 17:47:28 -0600

    Better tests for version strings

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,16 @@
 """Unit tests for version information."""
 
+import re
+
 from pingdat import version
 
 
 def test_basic_version():
     """Ensure basic version information is available."""
-    assert version.__pkgname__ is not None
-    assert version.__version__ is not None
+    assert version.__pkgname__
+    assert version.__version__
+
+
+def test_version_is_valid_string():
+    """Ensure version strings meet expected conventions."""
+    assert re.match(r"^[0-9]+(\.[0-9])+(-.*)?$", version.__version__)
