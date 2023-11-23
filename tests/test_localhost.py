8acb22b5b7a3caa41ba7b16eb3a0e48d991e1072
jheddings <jason@heddings.com>
2023-11-23 08:16:49 -0700

    Improve unit tests.

--- tests/test_localhost.py
+++ tests/test_localhost.py
@@ -1,13 +1,41 @@
 """Verify pingdat using localhost."""
 
+from time import sleep
+
+import pytest
+
 from pingdat import PingTarget
 
 
-def test_basic_ping():
-    target = PingTarget(name="ping::localhost", address="localhost", interval=0)
+@pytest.fixture
+def localhost():
+    """Create a PingTarget for localhost."""
 
-    delay = target()
+    target = PingTarget(name="ping::localhost", address="localhost", interval=1)
+
+    target.start()
+
+    yield target
+
+    target.stop()
+
+
+def test_basic_ping(localhost: PingTarget):
+    """Verify that a basic ping works."""
+
+    # give me a ping, Vasili
+    delay = localhost()
 
     assert delay is not None
     assert delay is not False
     assert delay < 1
+
+
+def test_ping_thread(localhost: PingTarget):
+    """Verify that the ping thread is running."""
+
+    # one ping only, please
+    sleep(0.25)
+
+    # this is kind of a hack to get number of requests, but it works
+    assert localhost.metrics.requests._value.get() == localhost.count
